fb0b60af67f1aa506d55a89d14f63ec38d5dea4f
Mazhar <mazhartuhin8291@gmail.com>
2026-06-26 22:25:18 +0600

    sdgathsgfnt

--- routes/note_routes.js
+++ routes/note_routes.js
@@ -2,14 +2,14 @@ const express = require('express')
 const { createNote, readNotes, readSingleNote, updateNote, deleteNote } = require('../controllers/notes_controller')
 const noteRoutes = express.Router()
 
-noteRoutes.post('/create', createNote)
+// noteRoutes.post('/create', createNote)
 
-noteRoutes.get('/read', readNotes)
+// noteRoutes.get('/read', readNotes)
 
-noteRoutes.get('/read/:id', readSingleNote)
+// noteRoutes.get('/read/:id', readSingleNote)
 
-noteRoutes.put('/update/:id', updateNote)
+// noteRoutes.put('/update/:id', updateNote)
 
-noteRoutes.delete('/delete/:id', deleteNote)
+// noteRoutes.delete('/delete/:id', deleteNote)
 
 module.exports = noteRoutes
